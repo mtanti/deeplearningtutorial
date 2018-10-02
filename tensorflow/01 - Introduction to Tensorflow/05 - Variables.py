1944934e9ff0b94afa9a42f635037b5e5f9f664b
mtanti <marctanti@gmail.com>
2018-10-02 08:57:58 +0200

    Create 05 - Variables.py

---
+++ tensorflow/01 - Introduction to Tensorflow/05 - Variables.py
@@ -0,0 +1,22 @@
+import tensorflow as tf
+
+g = tf.Graph()
+with g.as_default():
+    v = tf.get_variable('v', [], tf.float32)
+
+    v_in = tf.placeholder(tf.float32, [], 'v_in')
+    v_setter = tf.assign(v, v_in) #Run this to set v to v_in
+    
+    g.finalize()
+    tf.summary.FileWriter('.', g)
+
+    with tf.Session() as s:
+        s.run([ v_setter ], { v_in: 1.0 })
+        
+        [ result ] = s.run([ v ], { })
+        print(result)
+
+        s.run([ v_setter ], { v_in: 2.0 })
+
+        [ result ] = s.run([ v ], { })
+        print(result)
