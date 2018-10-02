6db9d325d4096e5d23634dc2c172bceaa8fb4698
mtanti <marctanti@gmail.com>
2018-10-02 08:56:16 +0200

    Create 04 - Variably sized tensors.py

---
+++ tensorflow/01 - Introduction to Tensorflow/04 - Variably sized tensors.py
@@ -0,0 +1,15 @@
+import tensorflow as tf
+
+g = tf.Graph()
+with g.as_default():
+    a = tf.placeholder(tf.float32, [None], 'a')
+    b = tf.placeholder(tf.float32, [None, 2], 'b')
+
+    g.finalize()
+    tf.summary.FileWriter('.', g)
+
+    with tf.Session() as s:
+        [ result ] = s.run([ a ], { a: [1.0] })
+        [ result ] = s.run([ a ], { a: [1.0, 2.0, 3.0] })
+        [ result ] = s.run([ b ], { b: [[1.0, 2.0]] })
+        [ result ] = s.run([ b ], { b: [[1.0,2.0], [3.0,4.0], [5.0,6.0]] })
